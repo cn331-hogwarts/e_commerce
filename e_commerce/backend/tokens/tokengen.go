c596570c37a8e263ff87dac8d99308f0e2e87b80
Augustus2011 <kun.kerdthaisong@gmail.com>
2023-12-23 19:31:06 +0700

    updates and install jwt

--- e_commerce/backend/tokens/tokengen.go
+++ e_commerce/backend/tokens/tokengen.go
@@ -1,9 +1,18 @@
-package tokens
+package token
 
 import (
+	"context"
+	"log"
+	"os"
 	"time"
 
+	"github/Augustus2011/ecommerce/database"
+
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -14,7 +23,10 @@ type SignedDetails struct {
 	jwt.StandardClaims
 }
 
-func TokenGenerator(email string, firstname string, lastname string, uid string) (signedToken string, signedfreshtoken string, err error) {
+var UserData *mongo.Collection = database.UserData(database.Client, "Users")
+var SECRET_KEY = os.Getenv("SECRET_LOVE")
+
+func TokenGenerator(email string, firstname string, lastname string, uid string) (signedtoken string, signedrefreshtoken string, err error) {
 	claims := &SignedDetails{
 		Email:      email,
 		First_Name: firstname,
@@ -26,17 +38,62 @@ func TokenGenerator(email string, firstname string, lastname string, uid string)
 	}
 	refreshclaims := &SignedDetails{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
 		},
 	}
-	token, _ := jwt.NewWithClaims
-
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+	refreshtoken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshclaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	return token, refreshtoken, err
 }
 
-func ValidateToken() {
+func ValidateToken(signedtoken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(signedtoken, &SignedDetails{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY), nil
+	})
 
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = "The Token is invalid"
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = "token is expired"
+		return
+	}
+	return claims, msg
 }
 
-func UpdateAllTokens() {
+func UpdateAllTokens(signedtoken string, signedrefreshtoken string, userid string) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var updateobj primitive.D
+	updateobj = append(updateobj, bson.E{Key: "token", Value: signedtoken})
+	updateobj = append(updateobj, bson.E{Key: "refresh_token", Value: signedrefreshtoken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateobj = append(updateobj, bson.E{Key: "updatedat", Value: updated_at})
+	upsert := true
+	filter := bson.M{"user_id": userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := UserData.UpdateOne(ctx, filter, bson.D{
+		{Key: "$set", Value: updateobj},
+	},
+		&opt)
+	defer cancel()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
 
 }
