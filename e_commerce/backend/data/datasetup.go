459fbe7c5dfc493054421479c5e209695582606d
Augustus2011 <kun.kerdthaisong@gmail.com>
2023-12-23 14:44:07 +0700

    update backend

---
+++ e_commerce/backend/data/datasetup.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBset() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TOTO(), nil)
+	if err != nil {
+		log.Println("fail to connect mongodb")
+		return nil
+	}
+	fmt.Println("Successfully connect to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
+
+}
