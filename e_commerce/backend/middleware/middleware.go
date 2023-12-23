b3732eed8c2f03ef16aeb6ec72aa97c04c16de1c
Augustus2011 <kun.kerdthaisong@gmail.com>
2023-12-23 21:05:33 +0700

    update

---
+++ e_commerce/backend/middleware/middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	token "github/Augustus2011/ecommerce/tokens"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "no authorization"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
